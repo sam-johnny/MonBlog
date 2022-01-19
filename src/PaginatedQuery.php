6862aef4879c8220b8486da82f6c13f859610a92
sam-johnny <sam.johnny@live.fr>
2022-01-19 18:55:22 +0100

    modify PaginatedQuery

--- src/PaginatedQuery.php
+++ src/PaginatedQuery.php
@@ -9,31 +9,42 @@ class PaginatedQuery
     private $query;
     private $queryCount;
     private $pdo;
-    private $perPage;
+    private $limitPage;
     private $count;
     private $items;
 
-    public function __construct(string $query, string $queryCount, \PDO $pdo = null, int $perPage = 4)
+    public function __construct(string $query, string $queryCount, int $limitPage, \PDO $pdo = null)
     {
         $this->query = $query;
         $this->queryCount = $queryCount;
-        $this->pdo = $pdo ?: DbConnect::getPDO();
-        $this->perPage = $perPage;
+        $this->limitPage = $limitPage;
+        $this->pdo = $pdo ?: Database::getPDO();
     }
 
     public function getItems(string $classMapping): array
     {
         if ($this->items === null) {
             $currentPage = $this->getCurrentPage();
-            $pages = $this->getPages();
-            if ($currentPage > $pages) {
+            $totalPages = $this->getTotalPages();
+            if ($currentPage > $totalPages) {
                 throw new \Exception("Cette page n'existe pas");
             }
-            $offset = $this->perPage * ($currentPage - 1);
-            $this->items = $this->pdo->query($this->query . " LIMIT {$this->perPage} OFFSET $offset")
+            $offset = $this->limitPage * ($currentPage - 1);
+            $this->items = $this->pdo->query($this->query . " LIMIT {$this->limitPage} OFFSET $offset")
                 ->fetchAll(PDO::FETCH_CLASS, $classMapping);
         }
         return $this->items;
+    }
+
+    public function numPage(string $link, int $i, ?string $active = null): ?string
+    {
+
+        $link .= "?page=";
+        $currentPage = $this->getCurrentPage();
+        $active = $currentPage === $i ? 'active' : '';
+        return <<<HTML
+<li class="page-item {$active}"><a class="page-link" href="{$link}{$i}">{$i}</a></li><br>
+HTML;
     }
 
     public function previousPage(string $link): ?string
@@ -42,18 +53,18 @@ class PaginatedQuery
         if ($currentPage <= 1) return null;
         if ($currentPage > 2) $link .= "?page=" . ($currentPage - 1);
         return <<<HTML
-<a class="page-link" href="{$link}">&laquo; Page précédente</a>
+<li class="page-item"><a class="page-link " href="{$link}">&laquo; Page précédente</a></li> 
 HTML;
     }
 
     public function nextPage(string $link): ?string
     {
         $currentPage = $this->getCurrentPage();
-        $pages = $this->getPages();
-        if ($currentPage >= $pages) return null;
+        $totalPages = $this->getTotalPages();
+        if ($currentPage >= $totalPages) return null;
         $link .= "?page=" . ($currentPage + 1);
         return <<<HTML
-<a class="page-link ml-auto" href="{$link}">Page suivante &raquo;</a>
+<li class="page-item"><a class="page-link float-end" href="{$link}">Page suivante &raquo;</a></li>
 HTML;
     }
 
@@ -62,12 +73,12 @@ HTML;
         return URL::getPositiveInt('page', 1);
     }
 
-    private function getPages(): int
+    public function getTotalPages(): int
     {
         if ($this->count === null) {
             $this->count = (int)$this->pdo->query($this->queryCount)->fetch(PDO::FETCH_NUM)[0];
         }
-        return ceil($this->count / $this->perPage);
+        return ceil($this->count / $this->limitPage);
     }
 
 }
