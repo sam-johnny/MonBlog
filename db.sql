0e4b74414188555e2f7aabec0d951a6c1ccc4ded
sam-johnny <sam.johnny@live.fr>
2022-01-22 19:25:14 +0100

    add date update in admin/post

--- db.sql
+++ db.sql
@@ -14,16 +14,38 @@ CREATE SCHEMA IF NOT EXISTS `db_myblog` DEFAULT CHARACTER SET utf8mb4 ;
 USE `db_myblog` ;
 
 -- -----------------------------------------------------
+-- Table `db_myblog`.`user`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `db_myblog`.`user` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `username` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    `email` VARCHAR(255) NOT NULL,
+    `role` VARCHAR(13) NOT NULL,
+    PRIMARY KEY (`id`))
+    ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
 -- Table `db_myblog`.`post`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `db_myblog`.`post` (
                                                   `id` INT NOT NULL AUTO_INCREMENT,
                                                   `title` VARCHAR(255) NOT NULL,
     `slug` VARCHAR(255) NOT NULL,
+    `chapo` TEXT(650000) NOT NULL,
     `content` TEXT(650000) NOT NULL,
-    `created_at` DATETIME NOT NULL,
-    PRIMARY KEY (`id`))
+    `create_at` DATETIME NOT NULL,
+    `user_id` INT NOT NULL,
+    PRIMARY KEY (`id`),
+    CONSTRAINT `fk_post_user1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `db_myblog`.`user` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE RESTRICT)
     ENGINE = InnoDB;
+
+CREATE INDEX `fk_post_user1_idx` ON `db_myblog`.`post` (`user_id` ASC) VISIBLE;
 
 
 -- -----------------------------------------------------
@@ -38,59 +60,56 @@ CREATE TABLE IF NOT EXISTS `db_myblog`.`category` (
 
 
 -- -----------------------------------------------------
--- Table `db_myblog`.`user`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `db_myblog`.`user` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `username` VARCHAR(255) NOT NULL,
-    `password` VARCHAR(255) NOT NULL,
-    `email` VARCHAR(255) NOT NULL,
-    `role` VARCHAR(13) NOT NULL,
-    PRIMARY KEY (`id`))
-    ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
 -- Table `db_myblog`.`post_category`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `db_myblog`.`post_category` (
                                                            `post_id` INT NOT NULL,
                                                            `category_id` INT NOT NULL,
                                                            PRIMARY KEY (`post_id`, `category_id`),
-    CONSTRAINT `fk_post`
+    CONSTRAINT `fk_table1_post`
     FOREIGN KEY (`post_id`)
     REFERENCES `db_myblog`.`post` (`id`)
-    ON DELETE CASCADE
-    ON UPDATE RESTRICT,
-    CONSTRAINT `fk_category`
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+    CONSTRAINT `fk_table1_category1`
     FOREIGN KEY (`category_id`)
     REFERENCES `db_myblog`.`category` (`id`)
     ON DELETE CASCADE
     ON UPDATE RESTRICT)
     ENGINE = InnoDB;
 
-CREATE INDEX `fk_category_idx` ON `db_myblog`.`post_category` (`category_id` ASC) VISIBLE;
+CREATE INDEX `fk_table1_category1_idx` ON `db_myblog`.`post_category` (`category_id` ASC) VISIBLE;
+
 
 -- -----------------------------------------------------
 -- Table `db_myblog`.`comment`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `db_myblog`.`comment` (
                                                      `id` INT NOT NULL AUTO_INCREMENT,
-                                                     `username` VARCHAR(45) NOT NULL,
-    `content` TEXT(650000) NOT NULL,
+                                                     `content` TEXT(650000) NOT NULL,
     `created_at` DATETIME NOT NULL,
-    `status` VARCHAR(100) NOT NULL,
-    `email` VARCHAR(255) NOT NULL,
+    `status` INT NOT NULL,
     `post_id` INT NOT NULL,
+    `user_id` INT NOT NULL,
     PRIMARY KEY (`id`),
     CONSTRAINT `fk_comments_post1`
     FOREIGN KEY (`post_id`)
     REFERENCES `db_myblog`.`post` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+    CONSTRAINT `fk_comment_user1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `db_myblog`.`user` (`id`)
     ON DELETE CASCADE
     ON UPDATE RESTRICT)
     ENGINE = InnoDB;
 
 CREATE INDEX `fk_comments_post1_idx` ON `db_myblog`.`comment` (`post_id` ASC) VISIBLE;
+
+CREATE INDEX `fk_comment_user1_idx` ON `db_myblog`.`comment` (`user_id` ASC) VISIBLE;
+
+
+
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
