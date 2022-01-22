ce4a24c61c5ce270d22cfd8d889da0fc4e8cc2d1
sam-johnny <sam.johnny@live.fr>
2022-01-22 19:37:26 +0100

    add chapo in posts

---
+++ views/post/cardComment.php
@@ -0,0 +1,12 @@
+<?php
+$userComment = $userTable->find($commentary->getUserID());
+?>
+<div class="col-md-12">
+    <div class="card mb-3 bg-light">
+        <div class="card-body">
+            <h5 class="card-title"><?= htmlentities($userComment->getUsername()) ?></h5>
+            <p class="text-muted fst-italic"><?= $commentary->getCreatedAt()->format('d/m/Y à H:i:s') ?>
+            <p><?= $commentary->getFormattedContent() ?></p>
+        </div>
+    </div>
+</div>
