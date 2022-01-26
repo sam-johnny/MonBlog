e52bbde2636d166cad3a1c216d62597b7c36a8d8
sam-johnny <sam.johnny@live.fr>
2022-01-26 21:44:15 +0100

    change directory favico.ico

--- views/layouts/default.php
+++ views/layouts/default.php
@@ -10,7 +10,7 @@
     <!--Boostrap-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="icon" href="/img/favicon.ico" />
+    <link rel="icon" href="/favicon.ico" />
     <title><?= isset($title) ? htmlentities($title) : 'Mon site' ?></title>
 
 </head>
